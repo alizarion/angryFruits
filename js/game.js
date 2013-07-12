7c352716a92b9601d3bc6608dc90d2541066f0a1
alizarion <selim@openlinux.fr>
2013-07-12 12:42:21 +0200

    first commit

--- js/game.js
+++ js/game.js
@@ -1,10 +1,67 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[x] + 'CancelAnimationFrame'] ||
+                window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+var mouse = {
+    x:0,
+    y:0,
+    down:false,
+    init:function(){
+       $("#gamecanvas").mousemove(mouse.mousemovehandler);
+        $('#gamecanvas').mousedown(mouse.mousedownhandler);
+        $('#gamecanvas').mouseup(mouse.mouseuphandler);
+        $('#gamecanvas').mouseout(mouse.mouseuphandler);
+    },
+    mousemovehandler:function(ev){
+        var offset = $('#gamecanvas').offset();
+        mouse.x = ev.pageX - offset.left;
+        mouse.y = ev.pageY - offset.top;
+        if(mouse.down){
+            mouse.dragging = true;
+        }
+        console.log('mouse x = '+mouse.x + ', mouse y = '+ mouse.y+' dragging = ' + mouse.dragging );
+    },
+    mousedownhandler:function(ev){
+        mouse.down = true;
+        mouse.downX = mouse.x;
+        mouse.downY = mouse.y;
+        //on annule l'action provoqué par l'evenement par defaut
+        ev.originalEvent.preventDefault();
+    },
+    mouseuphandler: function(ev){
+        mouse.down = false ;
+        mouse.dragging = false;
+    }
+
+}
+
 var game = {
 // Start initializing objects, preloading assets and display start screen
     init: function(){
 // Hide all game layers and display the start screen
         levels.init();
         loader.init();
-       loader.loadImage('img/icons/level.png');
+        mouse.init();
         $('.gamelayer').hide();
         $('#gamestartscreen').show();
 //Get handler for game canvas and context
@@ -14,8 +71,43 @@ var game = {
     showLevelScreen:function(){
         $('.gamelayer').hide();
         $('#levelselectscreen').show(600);
+    },
+    mode:"intro",
+    //coordonnée xy du lance pierre
+    slingshotX : 140,
+    slingshotY : 280,
+    start :function(){
+        $('.gamelayer').hide();
+        //afficher le jeu et le score
+        $('#gamecanvas').show();
+        $('#scorescreen').show();
+        game.mode = "intro";
+        game.offsetLeft=0;
+        game.ended = false;
+        game.animationFrame = window.requestAnimationFrame(game.animate,game.canvas);
+    },
+    handlePanning:function(){
+        game.offsetLeft++;
+    },
+    animate:function() {
+        //animer l'arriere plan
+
+        game.handlePanning();
+        //animer les personnages
+        //dessiner l'arriere plan en parallax scrolling
+        game.context.drawImage(game.currentLevel.backgroundImage,game.offsetLeft/4,0,640,480,0,0,640,480);
+        game.context.drawImage(game.currentLevel.foregroundImage,game.offsetLeft,0,640,480,0,0,640,480);
+        //dessiner le lance pierre
+        game.context.drawImage(game.slingshotImage,game.slingshotX-game.offsetLeft,game.slingshotY);
+        game.context.drawImage(game.slingshotFrontImage,game.slingshotX-game.offsetLeft,game.slingshotY);
+
+        if(!game.ended){
+            game.animationFrame = window.requestAnimationFrame(game.animate,game.canvas);
+        }
     }
+
 }
+
 var levels = {
 // Level data
     data:[
@@ -46,6 +138,27 @@ var levels = {
     },
 // Load all data and images for a specific level
     load:function(number){
+
+        //declare new current level
+        game.currentLevel = {number: number, hero:[]};
+        game.score = 0;
+        $('#score').html('Score '+game.score);
+        var level = levels.data[number];
+        // chargement des images du level
+        game.currentLevel.backgroundImage =  loader.loadImage("img/backgrounds/"+level.background+".png");
+        game.currentLevel.foregroundImage =  loader.loadImage("img/backgrounds/"+level.foreground+".png");
+        //fronde .. lasstack
+        game.slingshotImage = loader.loadImage("img/slingshot.png");
+        game.slingshotFrontImage = loader.loadImage("img/slingshot-front.png");
+
+        //lancer le jeu quand le chargement est términé
+        if(loader.loaded){
+            game.start();
+        }
+        else{
+            loader.onload = game.start;
+        }
+
     }
 }
 
